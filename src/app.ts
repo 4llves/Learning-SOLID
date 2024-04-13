af102ab174b517fcc6cc08e49bdb6851e30bf020
4llves <jhonatab4@gmail.com>
2024-04-13 18:51:17 -0300

    feat: install and config prisma

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,16 @@
-import fastify from "fastify"
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 // created app
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Alves Jhonata',
+    email: 'alves@email.com',
+  },
+})
+
+// ORM - Object Relational Mapper
