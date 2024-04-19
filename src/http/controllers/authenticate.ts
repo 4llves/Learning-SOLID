5c4c06711d582691f642659d31be78c35ca601c1
4llves <jhonatab4@gmail.com>
2024-04-19 13:50:30 -0300

    implementing: pattern factory

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -3,6 +3,7 @@ import { z } from 'zod'
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 import { AuthenticateUseCase } from '@/use-cases/authenticate'
 import { InvalidCredentialsError } from '@/use-cases/erros/invalid-credentials-error'
+import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case'
 
 export async function authenticate(req: FastifyRequest, rep: FastifyReply) {
   const authenticateBodySchema = z.object({
@@ -13,8 +14,7 @@ export async function authenticate(req: FastifyRequest, rep: FastifyReply) {
   const { email, password } = authenticateBodySchema.parse(req.body)
 
   try {
-    const usersRepository = new PrismaUsersRepository()
-    const authenticateUseCase = new AuthenticateUseCase(usersRepository)
+    const authenticateUseCase = makeAuthenticateUseCase()
 
     await authenticateUseCase.execute({
       email,
